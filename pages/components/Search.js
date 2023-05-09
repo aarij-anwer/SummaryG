024b0a78488628a5851fa7762c01ab2ac002e4a2
Andrew Brown <andrew.robert.brown@gmail.com>
2023-05-09 00:59:24 +0000

    Add front-end layout

--- pages/components/Search.js
+++ pages/components/Search.js
@@ -1,6 +1,20 @@
+import styles from '@/styles/Home.module.css'
+
 export default function Search(props) {
 
   return (
-      <div>Search</div>
+    <div className={styles.searchbar}>
+      <form className={styles.searchform} onSubmit={event => event.preventDefault()} autoComplete="off">
+        <input
+          className={styles.searchbox}
+          name="searchTerm"
+          type="text"
+          placeholder="Enter URL"
+          value={''}
+          onChange={''}
+        />
+      </form>
+      <button className={styles.searchbutton} onClick={''}>Search</button>
+    </div>
   );
 }
