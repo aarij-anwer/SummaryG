7675b81e94b1f2e30357a3504b0b360403260c67
Aarij Anwer <aarij.anwer@gmail.com>
2023-05-11 13:56:40 -0400

    add helper functions and refactor code

--- pages/api/openai.js
+++ pages/api/openai.js
@@ -5,21 +5,13 @@ const configuration = new Configuration({
 });
 const openai = new OpenAIApi(configuration);
 
-/*
-  - summary = 50 word executive summary
-  - review = three takeways (article) in 100 words, write a review of the movie in 100 words with one positive and one negative
-  - oneword = quote from the article/book/movie
-  - similar = similar article/movie/book
-*/
-
-//singularize the type
-async function generateCompletions(type, link) {
+//call openai with a prompt and max_tokens (size)
+async function generateCompletions(prompt, max_tokens) {
   const completion = await openai.createCompletion({
     model: "text-davinci-003",
-    // prompt: "Summarize this article:\n\nhttps://www.cbc.ca/news/politics/canada-new-passport-more-nature-fewer-history-1.6838308",
-    prompt: `For the ${type} ${link}, provide a 50 word executive summary.`,
+    prompt,
     temperature: 0.7,
-    max_tokens: 4000,
+    max_tokens,
     top_p: 1.0,
     frequency_penalty: 0.0,
     presence_penalty: 0.0,
@@ -27,14 +19,13 @@ async function generateCompletions(type, link) {
   return completion.data.choices[0].text;
 }
 
+//handler for the openai.js
 export default async function handler(req, res) {
-  // const { prompt } = req.query;
-
   const type = req.query.searchType;
-  const prompt = req.query.userInput;
+  const nameOrURL = req.query.userInput;
 
-  console.log(type);
-  console.log(prompt);
+  console.log("type", type);
+  console.log("nameOrURL", nameOrURL);
 
   if (!configuration.apiKey) {
     res.status(500).json({
@@ -46,14 +37,17 @@ export default async function handler(req, res) {
   }
 
   try {
-    const completions = await generateCompletions(type, prompt);
     //prompt for summary
+    let prompt = createSummaryPrompt(type, nameOrURL);
+    console.log("prompt", prompt);
+
+    const summary = await generateCompletions(prompt, 3000);
     //prompt for review
     //prompt for oneword
     //prompt for similar
 
-    console.log(completions);
-    res.status(200).json({ completions });
+    console.log("Summary", summary);
+    res.status(200).json({ summary });
   } catch (error) {
     if (error.response) {
       console.error(error.response.status, error.response.data);
@@ -68,3 +62,32 @@ export default async function handler(req, res) {
     }
   }
 }
+
+
+/*
+  - summary = 50 word executive summary
+  - review = three takeways (article) in 100 words, write a review of the movie in 100 words with one positive and one negative
+  - oneword = quote from the article/book/movie
+  - similar = similar article/movie/book
+*/
+
+const createSummaryPrompt = (type, nameOrURL) => {
+  //singularize the type
+  const singularizedType = singularizeType(type);
+  let answer = "";
+
+  if (singularizedType == 'article')
+  {
+    answer = `Write an executive summary of 50 words for the following article: ${nameOrURL}`;
+  } else if (singularizedType == 'book') {
+    answer = `Write an executive summary of 50 words for the following book: ${nameOrURL}`;
+  } else {
+    answer = `Write an executive summary of 50 words for the following movie: ${nameOrURL}`;
+  }
+
+  return answer;
+};
+
+const singularizeType = (type) => {
+  return type.substring(0,type.length-1);
+};
