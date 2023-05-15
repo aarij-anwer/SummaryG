c63ad73265320a092cecddddd3e2b1a93a598785
Andrew Brown <andrew.robert.brown@gmail.com>
2023-05-15 15:19:13 +0000

    Add loading screen spinner

--- pages/components/Loading.js
+++ pages/components/Loading.js
@@ -5,7 +5,10 @@ export default function Loading(props) {
   return (
     <div className={styles.loading  + ' ' + (!props.guruCognating ? styles.hidden : '')}>
       <img className={styles.loadingimage} src='/robot.png'/>
-      <span className={styles.loadingmessage}>Guru is Cognating!</span>
+      <div className={styles.loadingmessage}>
+        <span className={styles.loadingmessage}>Guru is Cognating!</span>
+        <img src='/openAI.png' className={styles.rotate} />
+      </div>
     </div>
   );
 }
