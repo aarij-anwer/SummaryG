bfed1f72234145846094fa3a0d756643f67cd186
egesenli <egesenli@gmail.com>
2023-05-17 11:45:17 -0400

    Style the content area

--- pages/components/Title.js
+++ pages/components/Title.js
@@ -2,7 +2,7 @@ export default function Title(props) {
 
   return (
     <div>
-      <span>Title:</span> {props.title}
+      <h1 class="text-4xl font-extrabold light:text-black">Title: {props.title}</h1>
     </div>
   );
 }
