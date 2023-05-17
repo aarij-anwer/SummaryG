d7b1369a9d47e55bd13fab784c951226ae14e9ee
egesenli <egesenli@gmail.com>
2023-05-17 12:23:14 -0400

    Style the search component

--- pages/components/Search.js
+++ pages/components/Search.js
@@ -1,6 +1,5 @@
 import { useRef } from "react"
 import axios from "axios";
-import styles from '@/styles/Home.module.css'
 
 export default function Search(props) {
   const inputRef = useRef();
@@ -32,23 +31,25 @@ export default function Search(props) {
     }
   };
 
-
   return (
-    <div className={styles.searchbar}>
-      <form
-        className={styles.searchform}
-        onSubmit={handleSubmit}
-        autoComplete="off"
-      >
+    <form onSubmit={handleSubmit} autoComplete="off">
+      <label htmlFor="default-search" className="mb-2 text-sm font-medium text-gray-900 sr-only dark:text-white">Search</label>
+      <div className="relative">
+        <div className="absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none">
+          <svg aria-hidden="true" className="w-5 h-5 text-gray-500 dark:text-gray-400" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+            <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"></path>
+          </svg>
+        </div>
         <input
-          className={styles.searchbox + ' ' + styles[props.searchType]}
-          name="searchTerm"
-          type="text"
+          id="default-search"
+          className="block w-full p-4 pl-10 text-sm text-gray-900 border border-gray-300 bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+          type="search"
           ref={inputRef}
           placeholder={props.searchType == "articles" ? "Enter article URL" : `Enter ${props.searchType.slice(0, -1)} title`}
+          required
         />
-      </form>
-      <button className={styles.searchbutton} onClick={handleSubmit}>Search</button>
-    </div>
+        <button type="submit" className="text-white absolute right-2.5 bottom-2.5 bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Search</button>
+      </div>
+    </form>
   );
 }
