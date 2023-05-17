36351648424a33c7af5faa66ada6137af3139aa8
egesenli <egesenli@gmail.com>
2023-05-17 14:22:43 -0400

    Adjust the button and searchbar colors based on selection

--- pages/components/Navigation.js
+++ pages/components/Navigation.js
@@ -1,4 +1,4 @@
-import { useState } from 'react';;
+import { useState } from 'react';
 import { FiMenu } from 'react-icons/fi';
 
 export default function Navigation(props) {
@@ -22,26 +22,27 @@ export default function Navigation(props) {
         <FiMenu className="h-6 w-6" />
       </button>
       <div
-        className={`${isOpen ? "" : "hidden"
-          } top-navbar w-full lg:inline-flex lg:flex-grow lg:w-auto`}
+        className={`${isOpen ? '' : 'hidden'} top-navbar w-full lg:inline-flex lg:flex-grow lg:w-auto`}
       >
         <div className="lg:inline-flex lg:flex-row lg:ml-auto lg:w-auto w-full lg:items-center items-start flex flex-col lg:h-auto">
           <button
-            className={`lg:inline-flex lg:w-auto w-full px-3 py-2 rounded text-white items-center justify-center hover:bg-gray-900 ${props.searchType === 'articles' ? 'bg-gray-900' : ''
+            className={`lg:inline-flex lg:w-auto w-full px-3 py-2 rounded text-white items-center justify-center hover:bg-gray-700 ${props.searchType === 'articles' ? 'bg-gray-700' : ''
               }`}
             onClick={() => props.onSearchTypeChange('articles')}
           >
             <span>Articles</span>
           </button>
+          <div className="mt-2 lg:mt-0 lg:w-1"></div> {/* Add a small distance */}
           <button
-            className={`lg:inline-flex lg:w-auto w-full px-3 py-2 rounded text-white items-center justify-center hover:bg-gray-900 ${props.searchType === 'movies' ? 'bg-gray-900' : ''
+            className={`lg:inline-flex lg:w-auto w-full px-3 py-2 rounded text-white items-center justify-center hover:bg-cyan-700 ${props.searchType === 'movies' ? 'bg-cyan-700' : ''
               }`}
             onClick={() => props.onSearchTypeChange('movies')}
           >
             <span>Movies</span>
           </button>
+          <div className="mt-2 lg:mt-0 lg:w-1"></div> {/* Add a small distance */}
           <button
-            className={`lg:inline-flex lg:w-auto w-full px-3 py-2 rounded text-white items-center justify-center hover:bg-gray-900 ${props.searchType === 'books' ? 'bg-gray-900' : ''
+            className={`lg:inline-flex lg:w-auto w-full px-3 py-2 rounded text-white items-center justify-center hover:bg-emerald-400 ${props.searchType === 'books' ? 'bg-emerald-400' : ''
               }`}
             onClick={() => props.onSearchTypeChange('books')}
           >
--- pages/components/Search.js
+++ pages/components/Search.js
@@ -1,4 +1,4 @@
-import { useRef } from "react"
+import { useRef } from "react";
 import axios from "axios";
 
 export default function Search(props) {
@@ -31,10 +31,22 @@ export default function Search(props) {
     }
   };
 
+  const getSearchBarColor = () => {
+    switch (props.searchType) {
+      case "articles":
+        return "bg-gray-50 dark:bg-gray-700";
+      case "movies":
+        return "bg-blue-50 dark:bg-cyan-700";
+      case "books":
+        return "bg-green-50 dark:bg-emerald-400";
+      default:
+        return "bg-gray-50 dark:bg-gray-700";
+    }
+  };
+
   return (
     <form onSubmit={handleSubmit} autoComplete="off">
-      <label htmlFor="default-search" className="mb-2 text-sm font-medium text-gray-900 sr-only dark:text-white">Search</label>
-      <div className="relative">
+      <div className={`relative ${getSearchBarColor()}`}>
         <div className="absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none">
           <svg aria-hidden="true" className="w-5 h-5 text-gray-500 dark:text-gray-400" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
             <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"></path>
@@ -42,13 +54,13 @@ export default function Search(props) {
         </div>
         <input
           id="default-search"
-          className="block w-full p-4 pl-10 text-sm text-gray-900 border border-gray-300 bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+          className={`block w-full p-4 pl-10 text-sm text-gray-900 bg-gray-700 focus:ring-blue-500 focus:border-blue-500 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 ${getSearchBarColor()}`}
           type="search"
           ref={inputRef}
-          placeholder={props.searchType == "articles" ? "Enter article URL" : `Enter ${props.searchType.slice(0, -1)} title`}
+          placeholder={props.searchType === "articles" ? "Enter article URL" : `Enter ${props.searchType.slice(0, -1)} title`}
           required
         />
-        <button type="submit" className="text-white absolute right-2.5 bottom-2.5 bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Search</button>
+        <button type="submit" className="text-white absolute right-2.5 top-.5 bottom-1.5 bg-transparent hover:bg-amber-500 text-gray-200 font-semibold hover:text-white py-2 px-4 border border-gray-200 hover:border-transparent rounded text-sm">Search</button>
       </div>
     </form>
   );
