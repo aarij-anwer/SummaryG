0894da0d7b0c95bb08e04e7e25604e64484e40e3
egesenli <egesenli@gmail.com>
2023-05-18 23:52:06 -0400

    Change the label to Takeaways for the articles

--- pages/components/Review.js
+++ pages/components/Review.js
@@ -1,15 +1,16 @@
 export default function Review(props) {
+  const label = props.searchType === "articles" ? "Takeaways" : "Review";
 
   return (
     <div>
       <label htmlFor="message" className="block mb-2 text-sm font-medium text-gray-900 dark:text-black">
-        Review:
+        {label}:
       </label>
       <div
         id="message"
         className="block p-2.5 h-[200px] text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 resize-y overflow-auto focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-        >
-          {props.review}
+      >
+        {props.review}
       </div>
     </div>
   );
