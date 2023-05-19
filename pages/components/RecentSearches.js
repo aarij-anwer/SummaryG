49c8690b867bba72e00cebd22aa27e0c348fdafb
egesenli <egesenli@gmail.com>
2023-05-18 20:12:13 -0400

    Replace class with className

--- pages/components/RecentSearches.js
+++ pages/components/RecentSearches.js
@@ -5,22 +5,22 @@ export default function RecentSearches(props) {
 
   return (
     <div className="sidebar top-0 bottom-0 lg:left-0 p-2 w-[300px] overflow-y-auto text-center bg-gray-900 hidden">
-      <div class="text-gray-100 text-xl">
-        <div class="p-2.5 mt-1 flex items-center">
-          <i class="bi bi-app-indicator px-2 py-1 rounded-md bg-blue-600"></i>
-          <h1 class="font-bold text-gray-200 text-[15px] ml-3">Recent Searches</h1>
+      <div className="text-gray-100 text-xl">
+        <div className="p-2.5 mt-1 flex items-center">
+          <i className="bi bi-app-indicator px-2 py-1 rounded-md bg-blue-600"></i>
+          <h1 className="font-bold text-gray-200 text-[15px] ml-3">Recent Searches</h1>
         </div>
-        <div class="my-2 bg-gray-600 h-[1px]"></div>
+        <div className="my-2 bg-gray-600 h-[1px]"></div>
       </div>
 
-      <div id="recentSearches" class="text-gray-100">
+      <div id="recentSearches" className="text-gray-100">
         {props.searches?.map((search) => (
           <div
             key={search.id}
-            class="p-2.5 mt-3 flex items-center rounded-md px-4 duration-300 cursor-pointer hover:bg-blue-600 text-white"
+            className="p-2.5 mt-3 flex items-center rounded-md px-4 duration-300 cursor-pointer hover:bg-blue-600 text-white"
             onClick={() => handleSearchSelect(search.id)}
           >
-            <span class="text-[15px] ml-4 text-gray-200 font-bold">{search.searchTerm}</span>
+            <span className="text-[15px] ml-4 text-gray-200 font-bold">{search.searchTerm}</span>
           </div>
         ))}
       </div>
