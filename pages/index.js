d3af3b9a7a6a7a91422e151e7f420701f788f6a6
egesenli <egesenli@gmail.com>
2023-05-18 23:50:33 -0400

    Set the searchType state for Review component

--- pages/index.js
+++ pages/index.js
@@ -123,7 +123,7 @@ export default function Home() {
               <br/>
               <Summary summary={summary} />
               <br/>
-              <Review review={review} />
+              <Review review={review} searchType={searchState} />
             </div>
             <div className={"relative overflow-x-auto shadow-md min-w-[200px] max-w-[400px] sm:w-auto w-1/4 mt-4 pr-4 pl-4 text-sm text-left text-gray-500 dark:text-gray-400" + (!searchIdState ? 'hidden' : '')}>
               <br/>
