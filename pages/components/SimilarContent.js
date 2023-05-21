dc881175ed610f901409f86242916098c6e0974e
Aarij Anwer <94186270+aarij-anwer@users.noreply.github.com>
2023-05-21 14:18:07 -0400

    enable similar content to open up in new tab/window for articles

--- pages/components/SimilarContent.js
+++ pages/components/SimilarContent.js
@@ -9,7 +9,7 @@ export default function SimilarContent(props) {
         id="message"
         className="block p-2.5 h-[200px] text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 resize-y overflow-auto focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
         >
-          {props.similarContent?.includes("google") ? (<a href={props.similarContent}>Click for Similar Content</a>) : props.similarContent}
+          {props.similarContent?.includes("google") ? (<a href={props.similarContent} target="_blank">Click Here for Similar Content</a>) : props.similarContent}
       </div>
     </div>
   );
