901cc75f29daa8624ffded7d7989a9a0bd6b6a2b
egesenli <egesenli@gmail.com>
2023-05-21 23:49:08 -0400

    Insert html-react-parser and create giveStyle function to style inside of the list

--- pages/components/Review.js
+++ pages/components/Review.js
@@ -1,5 +1,18 @@
+import parse, { domToReact } from 'html-react-parser';
+
 export default function Review(props) {
   const label = props.similarContent?.includes("google") ? "Takeaways" : "Review";
+  const giveStyle = {
+    replace: ({ children, name }) => {
+      if (name === 'ol') {
+        return (
+          <ol className="list-decimal list-inside">
+            {domToReact(children, giveStyle)}
+          </ol>
+        );
+      }
+    },
+  };
 
   return (
     <div>
@@ -10,7 +23,7 @@ export default function Review(props) {
         id="message"
         className="block p-2.5 h-[200px] text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 resize-y overflow-auto focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
       >
-        {props.review}
+        {typeof props.review === "string" ? parse(props.review, giveStyle) : props.review}
       </div>
     </div>
   );
