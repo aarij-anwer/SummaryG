9a51d033895a7122e7c9718c283e49c35f3c9cb0
Aarij Anwer <94186270+aarij-anwer@users.noreply.github.com>
2023-06-06 16:39:38 -0600

    ensure props have loaded

--- pages/components/InitialLoad.js
+++ pages/components/InitialLoad.js
@@ -33,15 +33,15 @@ export default function InitialLoad(props) {
     <div className={`flex justify-evenly items-center w-full h-full ${props.searchIdState ? 'hidden' : ''}`}>
       <div>
         <p className="text-4xl text-black">Enter a URL for a rich summary of an article, or the name of a movie/book for a review!</p><p>&nbsp;</p>
-        <p className="text-xl text-black">Looking for inspiraton? Click on the article below&nbsp;
-        <span className="inline-block animate-bounce rounded-full p-1 bg-teal-400 text-white text-sm">
-          <svg className="w-6 h-6 mx-auto" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M19 13l-7 7-7-7m14-8l-7 7-7-7" />
-          </svg>
-        </span> and watch SummaryAI work!</p>
-        <form onSubmit={handleSubmit} autoComplete="off">
-          <button className="text-blue" type="submit">{props.suggested?.title}</button>
-        </form>
+        {props.suggested?.title && (
+        <><p className="text-xl text-black">Looking for inspiraton? Click on the article below&nbsp;
+            <span className="inline-block animate-bounce rounded-full p-1 bg-teal-400 text-white text-sm">
+              <svg className="w-6 h-6 mx-auto" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M19 13l-7 7-7-7m14-8l-7 7-7-7" />
+              </svg>
+            </span> and watch SummaryAI work!</p><form onSubmit={handleSubmit} autoComplete="off">
+              <button className="text-blue" type="submit">{props.suggested?.title}</button>
+            </form></>)}
       </div>
     </div>
   );
