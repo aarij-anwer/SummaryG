010737576ca18a1ca01dc7b13005bfb015ef85f8
Aarij Anwer <94186270+aarij-anwer@users.noreply.github.com>
2023-06-12 13:30:01 -0400

    fix the DB

--- pages/api/searchAPI.js
+++ pages/api/searchAPI.js
@@ -1,54 +1,86 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import { PrismaClient } from '@prisma/client';
 
+const prisma = new PrismaClient();
+
 export default async function handler(req, res) {
 
   if (req.method === "GET") {
     const searchID = req.query.searchIdState;
-
     console.log(searchID);
 
-    if (searchID) {
-      const prisma = new PrismaClient();
-      const content = await prisma.Result.findUnique({
-        where: {
-          searchID: parseInt(searchID, 10),
-        },
-      })
-
-      const title = await prisma.Search.findUnique({
-        where: {
-          id: {
-            equals: searchID
-          }
-        }
-      })
-
-      let updatedTitle = title[0].searchTerm;
-      let type = title[0].type;
-
-      // Check the search type and modify the title accordingly
-      if (title[0].type !== "articles") {
-        updatedTitle = capitalizeInitials(updatedTitle);
-      }
-
-      content[0].title = updatedTitle;
-      content[0].type = type;
-
-      console.log('content = ');
-      console.log(content);
-      res.send(JSON.stringify({ content: content }))
-    } else {
+    try {
+      const result = await getSearchAndResultBySearchID(parseInt(searchID),10);
+      console.log("Result in DB call", result);
+      res.send({ result });
+    } catch (error) {
+      console.log("Error in DB call", error);
       res.send({ name: 'John Doe' });
+    } finally {
     }
   }
+}
 
-  // Function to capitalize the initials of each word in the title
-  function capitalizeInitials(title) {
-    const words = title.split(" ");
-    for (let i = 0; i < words.length; i++) {
-      words[i] = words[i].charAt(0).toUpperCase() + words[i].substring(1).toLowerCase();
+// if (searchID) {
+//   const content = await prisma.Result.findUnique({
+//     where: {
+//       searchID: parseInt(searchID, 10),
+//     },
+//   })
+
+//   const title = await prisma.Search.findUnique({
+//     where: {
+//       id: {
+//         equals: searchID
+//       }
+//     }
+//   })
+
+//   let updatedTitle = title[0].searchTerm;
+//   let type = title[0].type;
+
+// // Check the search type and modify the title accordingly
+// if (title[0].type !== "articles") {
+//   updatedTitle = capitalizeInitials(updatedTitle);
+// }
+
+// content[0].title = updatedTitle;
+// content[0].type = type;
+
+// console.log('content = ');
+// console.log(content);
+// res.send(JSON.stringify({ content: content }))
+//   res.send({ name: 'John Doe' });
+// } else {
+//   res.send({ name: 'John Doe' });
+// }
+// }
+
+async function getSearchAndResultBySearchID(searchID) {
+  const searchAndResult = await prisma.search.findUnique({
+    where: { id: searchID },
+    select: {
+      searchTerm: true,
+      type: true,
+      Result: {
+        select: {
+          summary: true,
+          review: true,
+          oneWordReview: true,
+          similar: true
+        }
+      }
     }
-    return words.join(" ");
+  });
+
+  return searchAndResult;
+}
+
+// Function to capitalize the initials of each word in the title
+function capitalizeInitials(title) {
+  const words = title.split(" ");
+  for (let i = 0; i < words.length; i++) {
+    words[i] = words[i].charAt(0).toUpperCase() + words[i].substring(1).toLowerCase();
   }
+  return words.join(" ");
 }
